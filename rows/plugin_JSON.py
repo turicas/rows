28f963fe1719cd24da82f402c1db992f104b56f4
Flávio Codeço Coelho <fccoelho@gmail.com>
2014-05-12 09:05:36 -0300

    export_to_JSON complete

--- rows/plugin_JSON.py
+++ rows/plugin_JSON.py
@@ -1,8 +1,19 @@
 #!/usr/bin/env python
 # coding: utf-8
-
+"""
+This plugin exports to a JSON file formatted according to the specifications of Google Bigquery
+"""
 import json
 
-def write(table, filename=None):
-    with open(filename, 'w'):
-        json.dumps(table)
+__all__ = ['import_from_JSON', 'export_to_JSON']
+
+def export_to_JSON(table, filename=None):
+    base_object = {'rows': []}
+    for row in table:
+        base_object['rows'].append({'json': row})
+    if filename is not None:
+        with open(filename, 'w') as f:
+            f.write(json.dumps(base_object))
+    else:
+        return json.dumps(base_object)
+
