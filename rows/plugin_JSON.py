04c706a6491849fbaf5cc6e98ddc4b19c604a418
Flávio Codeço Coelho <fccoelho@gmail.com>
2014-05-12 09:26:12 -0300

    import_from_JSON complete

--- rows/plugin_JSON.py
+++ rows/plugin_JSON.py
@@ -4,6 +4,7 @@
 This plugin exports to a JSON file formatted according to the specifications of Google Bigquery
 """
 import json
+from .rows import LazyTable, Table
 
 __all__ = ['import_from_JSON', 'export_to_JSON']
 
@@ -16,4 +17,17 @@ def export_to_JSON(table, filename=None):
             f.write(json.dumps(base_object))
     else:
         return json.dumps(base_object)
+
+def import_from_JSON(JSON, lazy=False):
+    fields = JSON['rows'][0]['json'].keys()
+    rows_iter = (row['json'] for row in JSON['rows'])
+
+    if lazy:
+        table = LazyTable(iterable=rows_iter, fields=fields)
+    else:
+        table = Table(fields=fields)
+        table._rows = list(rows_iter)
+    return table
+
+
 
