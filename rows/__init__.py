1d9627c018cc14fa1a2ef133191a99a22af7eaa6
Álvaro Justen (@turicas) <alvarojusten@gmail.com>
2015-06-02 20:06:58 -0300

    Add `rows.import_from_xls`

--- rows/__init__.py
+++ rows/__init__.py
@@ -162,3 +162,52 @@ def export_to_csv(table, filename, encoding='utf-8'):
                                  for field, type_ in fields])
 
 
+# XLS plugin
+
+def import_from_xls(filename, fields=None, sheet_name=None, sheet_index=0,
+                    start_row=0, start_column=0):
+    import xlrd
+
+    book = xlrd.open_workbook(filename)
+    if sheet_name is not None:
+        sheet = book.sheet_by_name(sheet_name)
+    else:
+        sheet = book.sheet_by_index(sheet_index)
+    # TODO: may re-use Excel data types
+    cell_value = lambda row, col: unicode(sheet.cell_value(row, col)).strip()
+
+    # Get field names
+    # TODO: may use sheet.col_values or even sheet.ncols
+    column_count = 0
+    header = []
+    column_value = cell_value(start_row, start_column + column_count)
+    while column_value:
+        header.append(slug(column_value).lower())
+        column_count += 1
+        try:
+            column_value = cell_value(start_row, start_column + column_count)
+        except IndexError:
+            column_value = ''
+    header = [field.encode('utf-8') for field in header]
+
+    # Get sheet rows
+    # TODO: may use sheel.col_slice or even sheet.nrows
+    table_rows = []
+    row_count = 0
+    start_row += 1
+    cell_is_empty = False
+    while not cell_is_empty:
+        row = [cell_value(start_row + row_count, start_column + column_index)
+               for column_index in range(column_count)]
+        cell_is_empty = ''.join([field.strip() for field in row]) == ''
+        if not cell_is_empty:
+            table_rows.append(row)
+            row_count += 1
+
+    if fields is None:
+        fields = detect_field_types(header, table_rows, encoding='utf-8')
+    table = Table(fields=fields)
+    for row in table_rows:
+        table.append({field_name: value
+                      for field_name, value in zip(header, row)})
+    return table
