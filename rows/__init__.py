fb0cbcb8a97a556ff167bbe562fb2a946fbaa0c4
Álvaro Justen (@turicas) <alvarojusten@gmail.com>
2015-06-06 01:52:48 -0300

    Add HTML plugin (needs tests)

--- rows/__init__.py
+++ rows/__init__.py
@@ -211,3 +211,75 @@ def import_from_xls(filename, fields=None, sheet_name=None, sheet_index=0,
         table.append({field_name: value
                       for field_name, value in zip(header, row)})
     return table
+
+
+# HTML plugin
+
+import HTMLParser
+
+from lxml.etree import HTML as html_element_tree, tostring as to_string
+
+
+html_parser = HTMLParser.HTMLParser()
+
+def import_from_html(html, fields=None, table_index=0, include_fields=None,
+                     exclude_fields=None, converters=None, force_types=None):
+    # TODO: unescape before returning
+    # html = html_parser.unescape(html.decode(encoding))
+
+    html_tree = html_element_tree(html)
+    table_tree = html_tree.xpath('//table')
+    try:
+        table_tree = table_tree[table_index]
+    except IndexError:
+        raise IndexError('Table index {} not found'.format(table_index))
+
+    table_html = to_string(table_tree)
+    tr_elements = html_element_tree(table_html).xpath('//tr')
+    table_rows = []
+    for tr_element in tr_elements:
+        td_elements = html_element_tree(to_string(tr_element)).xpath('//td')
+        new_row = []
+        for td_element in td_elements:
+            data = u'\n'.join([x.strip()
+                    for x in list(td_element.itertext(with_tail=False))])
+            new_row.append(data)
+        table_rows.append(new_row)
+
+    # TODO: lxml -> unicode?
+
+    # TODO: unescape
+    if fields is None:
+        header = [x.strip() for x in table_rows[0]]
+        table_rows = table_rows[1:]
+        fields = detect_field_types(header, table_rows, encoding='utf-8')
+
+    table = Table(fields=fields)
+    for row in table_rows:
+        table.append({field_name: value
+                      for field_name, value in zip(header, row)})
+    return table
+
+
+# TODO: replace 'None' with '' on export_to_*
+def export_to_html(table, filename=None, encoding='utf-8'):
+    fields = table.fields.keys()
+    result = [u'<table>', u'', u'  <thead>', u'    <tr>']
+    header = [u'      <th>{}</th>'.format(field) for field in fields]
+    result.extend(header)
+    result.extend([u'    </tr>', u'  </thead>', u'', u'  <tbody>', u''])
+    for index, row in enumerate(table, start=1):
+        css_class = u'odd' if index % 2 == 1 else u'even'
+        result.append(u'    <tr class="{}">'.format(css_class))
+        for field in fields:
+            value = table.fields[field].serialize(getattr(row, field))
+            result.append(u'      <td>{}</td>'.format(value))
+        result.extend([u'    </tr>', u''])
+    result.extend([u'  </tbody>', u'</table>', u''])
+    html = u'\n'.join(result)
+
+    if filename is not None:
+        with open(filename, 'w') as fobj:
+            fobj.write(html.encode(encoding))
+    else:
+        return html
