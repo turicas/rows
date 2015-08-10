32a87f9a7a6f860385c047689157485411f15048
Álvaro Justen (@turicas) <alvarojusten@gmail.com>
2015-08-10 03:54:52 -0300

    Add Table.__add__

--- rows/__init__.py
+++ rows/__init__.py
@@ -47,6 +47,26 @@ class Table(object):
 
         return self.Row(*self._rows[item])
 
+    def __radd__(self, other):
+        if other == 0:
+            return self
+        raise ValueError()
+
+
+    def __add__(self, other):
+        if other == 0:
+            return self
+
+        if type(self) != type(other) or self.fields != other.fields:
+            raise ValueError('Tables have incompatible fields')
+
+        table = Table(fields=self.fields)
+        for row in self:
+            table.append({field: getattr(row, field) for field in row._fields})
+        for row in other:
+            table.append({field: getattr(row, field) for field in row._fields})
+        return table
+
 
 def detect_field_types(field_names, sample_rows, *args, **kwargs):
     """Where the magic happens"""
