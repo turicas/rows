a817ba37589d13dc42f2373409d5137ffe2e02b0
Álvaro Justen (@turicas) <alvarojusten@gmail.com>
2015-08-29 07:31:35 -0300

    Implement CLI 'sort' subcommand

--- rows/cli.py
+++ rows/cli.py
@@ -73,6 +73,28 @@ def join(input_encoding, output_encoding, input_locale, output_locale, keys,
         export_to_uri(destination, result)
 
 
+@cli.command(help='Sort from `source` by `key(s)` and save into `destination`')
+@click.option('--input-encoding', default='utf-8')
+@click.option('--output-encoding', default='utf-8')
+@click.option('--input-locale', default='en_US.UTF-8')
+@click.option('--output-locale', default='en_US.UTF-8')
+@click.argument('key')
+@click.argument('source')
+@click.argument('destination')
+def sort(input_encoding, output_encoding, input_locale, output_locale, key,
+         source, destination):
+    input_locale = input_locale.split('.')
+    output_locale = output_locale.split('.')
+    key = key.replace('^', '-')
+
+    with rows.locale_context(input_locale):
+        table = import_from_uri(source)
+        table.order_by(key)
+
+    with rows.locale_context(output_locale):
+        export_to_uri(destination, table)
+
+
 @cli.command(help='Sum tables from `source` URIs and save into `destination`')
 @click.option('--input-encoding', default='utf-8')
 @click.option('--output-encoding', default='utf-8')
