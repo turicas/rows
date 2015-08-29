32ea347c4280c7d8bd327f4e265fffcdc19fdeb1
Álvaro Justen (@turicas) <alvarojusten@gmail.com>
2015-08-29 01:34:40 -0300

    Add try/except ImportError on plugin imports

--- rows/__init__.py
+++ rows/__init__.py
@@ -9,6 +9,17 @@ from rows.localization import locale_context
 
 # Plugin imports
 
-from rows.plugins.csv import import_from_csv, export_to_csv
-from rows.plugins.xls import import_from_xls, export_to_xls
-from rows.plugins.html import import_from_html, export_to_html
+try:
+    from rows.plugins.csv import import_from_csv, export_to_csv
+except ImportError:
+    pass
+
+try:
+    from rows.plugins.xls import import_from_xls, export_to_xls
+except ImportError:
+    pass
+
+try:
+    from rows.plugins.html import import_from_html, export_to_html
+except ImportError:
+    pass
