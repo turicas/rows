a7cf539ea027ce36ec9e5179fba497cfaa557fbc
Álvaro Justen (@turicas) <alvarojusten@gmail.com>
2015-09-02 14:05:35 -0300

    PEP8-fix setup.py

--- setup.py
+++ setup.py
@@ -12,29 +12,30 @@ EXTRA_REQUIREMENTS['all'] = sum(EXTRA_REQUIREMENTS.values(), [])
 INSTALL_REQUIREMENTS = EXTRA_REQUIREMENTS['csv'] + EXTRA_REQUIREMENTS['cli']
 
 setup(name='rows',
-    description='A common, beautiful interface to tabular data, no matter the format',
-    long_description='',
-    version='0.2.0-dev',
-    author=u'Álvaro Justen',
-    author_email='alvarojusten@gmail.com',
-    url='https://github.com/turicas/rows/',
-    packages=['rows', 'rows.plugins'],
-    install_requires=INSTALL_REQUIREMENTS,
-    extras_require=EXTRA_REQUIREMENTS,
-    keywords=['tabular', 'table', 'csv', 'xls', 'html', 'rows'],
-    entry_points = {
-        'console_scripts': [
-            'rows = rows.cli:cli',
-            ],
-    },
-    classifiers = [
-        'Development Status :: 3 - Alpha',
-        'Intended Audience :: Developers',
-        'Intended Audience :: Science/Research',
-        'License :: OSI Approved :: GNU General Public License (GPL)',
-        'Natural Language :: English',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python :: 2.7',
-        'Topic :: Software Development :: Libraries :: Python Modules',
-    ]
+      description=('A common, beautiful interface to tabular data, '
+                   'no matter the format'),
+      long_description='',  # TODO
+      version='0.2.0-dev',
+      author=u'Álvaro Justen',
+      author_email='alvarojusten@gmail.com',
+      url='https://github.com/turicas/rows/',
+      packages=['rows', 'rows.plugins'],
+      install_requires=INSTALL_REQUIREMENTS,
+      extras_require=EXTRA_REQUIREMENTS,
+      keywords=['tabular', 'table', 'csv', 'xls', 'html', 'rows'],
+      entry_points = {
+          'console_scripts': [
+              'rows = rows.cli:cli',
+              ],
+      },
+      classifiers = [
+          'Development Status :: 3 - Alpha',
+          'Intended Audience :: Developers',
+          'Intended Audience :: Science/Research',
+          'License :: OSI Approved :: GNU General Public License (GPL)',
+          'Natural Language :: English',
+          'Operating System :: OS Independent',
+          'Programming Language :: Python :: 2.7',
+          'Topic :: Software Development :: Libraries :: Python Modules',
+      ]
 )
