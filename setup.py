deae59445f0ad8b7753c3860b115cf4134276d8c
Álvaro Justen (@turicas) <alvarojusten@gmail.com>
2016-08-13 21:57:02 -0300

    Add six as dependency

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ EXTRA_REQUIREMENTS = {
         'xpath': ['lxml'],
         'detect': ['file-magic'], }
 EXTRA_REQUIREMENTS['all'] = sum(EXTRA_REQUIREMENTS.values(), [])
-INSTALL_REQUIREMENTS = EXTRA_REQUIREMENTS['csv']
+INSTALL_REQUIREMENTS = ['six'] + EXTRA_REQUIREMENTS['csv']
 LONG_DESCRIPTION = '''
 No matter in which format your tabular data is: rows will import it,
 automatically detect types and give you high-level Python objects so you can
