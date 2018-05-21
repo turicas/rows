e0f2fe5a51816d65d51310ed52d97adc4e56919c
Álvaro Justen (@turicas) <alvarojusten@gmail.com>
2018-05-21 04:46:30 -0300

    Add rects-boundaries algorithm and refactor

--- rows/plugins/plugin_pdf.py
+++ rows/plugins/plugin_pdf.py
@@ -1,6 +1,6 @@
 # coding: utf-8
 
-# Copyright 2014-2017 Álvaro Justen <https://github.com/turicas/rows/>
+# Copyright 2014-2018 Álvaro Justen <https://github.com/turicas/rows/>
 
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU Lesser General Public License as published by
@@ -21,6 +21,7 @@ import io
 import logging
 from collections import defaultdict
 
+from cached_property import cached_property
 from pdfminer.converter import PDFPageAggregator, TextConverter
 from pdfminer.layout import LAParams, LTTextBox, LTTextLine, LTChar, LTRect
 from pdfminer.pdfdocument import PDFDocument
@@ -42,45 +43,28 @@ def number_of_pages(filename):
         return resolve1(document.catalog['Pages'])['Count']
 
 
-class Group(object):
-    'Helper class to group objects based on its positions and sizes'
-
-    def __init__(self, minimum=float('inf'), maximum=float('-inf'), threshold=0.5):
-        self.minimum = minimum
-        self.maximum = maximum
-        self.threshold = threshold
-        self.objects = []
-
-    @property
-    def min(self):
-        return self.minimum - self.threshold
-
-    @property
-    def max(self):
-        return self.maximum + self.threshold
-
-    def contains(self, obj):
-        return (self.min <= getattr(obj, self.dimension_0) <= self.max or
-                self.min <= getattr(obj, self.dimension_1) <= self.max)
-
-    def add(self, obj):
-        self.objects.append(obj)
-        d0 = getattr(obj, self.dimension_0)
-        d1 = getattr(obj, self.dimension_1)
-        if d0 < self.minimum:
-            self.minimum = d0
-        if d1 > self.maximum:
-            self.maximum = d1
+def _get_pdf_document(fobj):
+    parser = PDFParser(fobj)
+    doc = PDFDocument(parser)
+    parser.set_document(doc)
+    assert doc.is_extractable
+    return doc
 
 
-class HorizontalGroup(Group):
-    dimension_0 = 'y0'
-    dimension_1 = 'y1'
+def pdf_to_text(filename_or_fobj, page_numbers=None):
+    """Extract all text objects from a PDF file"""
 
-
-class VerticalGroup(Group):
-    dimension_0 = 'x0'
-    dimension_1 = 'x1'
+    filename, fobj = get_filename_and_fobj(filename_or_fobj, mode='rb')
+    doc = _get_pdf_document(fobj)
+    for page_number, page in enumerate(PDFPage.create_pages(doc), start=1):
+        if page_numbers is None or page_number in page_numbers:
+            rsrcmgr = PDFResourceManager()
+            laparams = LAParams()
+            result = io.StringIO()
+            device = TextConverter(rsrcmgr, result, laparams=laparams)
+            interpreter = PDFPageInterpreter(rsrcmgr, device)
+            interpreter.process_page(page)
+            yield result.getvalue()
 
 
 def get_delimiter_function(value):
@@ -100,14 +84,11 @@ def pdf_objects(fobj, page_numbers, starts_after=None, ends_before=None,
                 desired_types=(LTTextBox, LTTextLine, LTChar, LTRect)):
     'For each page inside a PDF, return the list of text objects'
 
+    doc = _get_pdf_document(fobj)
     rsrcmgr = PDFResourceManager()
     laparams = LAParams()
     device = PDFPageAggregator(rsrcmgr, laparams=laparams)
     interpreter = PDFPageInterpreter(rsrcmgr, device)
-    parser = PDFParser(fobj)
-    doc = PDFDocument(parser)
-    parser.set_document(doc)
-    assert doc.is_extractable
 
     started, finished = False, False
     if starts_after is None:
@@ -141,13 +122,55 @@ def pdf_objects(fobj, page_numbers, starts_after=None, ends_before=None,
     fobj.close()
 
 
-def group_objects(objs, get_attr_function, group_size, axis):
+class Group(object):
+    'Helper class to group objects based on its positions and sizes'
+
+    def __init__(self, minimum=float('inf'), maximum=float('-inf'), threshold=0):
+        self.minimum = minimum
+        self.maximum = maximum
+        self.threshold = threshold
+        self.objects = []
+
+    @property
+    def min(self):
+        return self.minimum - self.threshold
+
+    @property
+    def max(self):
+        return self.maximum + self.threshold
+
+    def contains(self, obj):
+        d0 = getattr(obj, self.dimension_0)
+        d1 = getattr(obj, self.dimension_1)
+        middle = d0 + (d1 - d0) / 2.0
+        return self.min <= middle <= self.max
+
+    def add(self, obj):
+        self.objects.append(obj)
+        d0 = getattr(obj, self.dimension_0)
+        d1 = getattr(obj, self.dimension_1)
+        if d0 < self.minimum:
+            self.minimum = d0
+        if d1 > self.maximum:
+            self.maximum = d1
+
+
+class HorizontalGroup(Group):
+    dimension_0 = 'y0'
+    dimension_1 = 'y1'
+
+
+class VerticalGroup(Group):
+    dimension_0 = 'x0'
+    dimension_1 = 'x1'
+
+
+def group_objects(objs, threshold, axis):
     if axis == 'x':
         GroupClass = VerticalGroup
     elif axis == 'y':
         GroupClass = HorizontalGroup
 
-    objs.sort(key=get_attr_function)
     groups = []
     for obj in objs:
         found = False
@@ -157,30 +180,10 @@ def group_objects(objs, get_attr_function, group_size, axis):
                 found = True
                 break
         if not found:
-            group = GroupClass(threshold=group_size)
+            group = GroupClass(threshold=threshold)
             group.add(obj)
             groups.append(group)
     return {group.minimum: group.objects for group in groups}
-
-
-def define_intervals(objs, min_attr, max_attr, threshold, axis):
-    groups = group_objects(objs, min_attr, threshold, axis)
-
-    intervals = [(key, max_attr(max(value, key=max_attr)))
-                 for key, value in groups.items()]
-    intervals.sort()
-    if not intervals:
-        return []
-
-    # Merge overlapping intervals
-    result = [intervals[0]]
-    for current in intervals[1:]:
-        previous = result.pop()
-        if current[0] <= previous[1] or current[1] <= previous[1]:
-            result.append((previous[0], max((previous[1], current[1]))))
-        else:
-            result.extend((previous, current))
-    return result
 
 
 def contains_or_overlap(a, b):
@@ -194,6 +197,147 @@ def contains_or_overlap(a, b):
                (x1min <= x2max <= x1max and y1min <= y2min <= y1max) or \
                (x1min <= x2max <= x1max and y1min <= y2max <= y1max)
     return contains or overlaps
+
+
+class ExtractionAlgorithm(object):
+
+    def __init__(self, objects, text_objects, x_threshold, y_threshold):
+        self.objects = objects
+        self.text_objects = text_objects
+        self.x_threshold = x_threshold
+        self.y_threshold = y_threshold
+
+    @property
+    def table_bbox(self):
+        raise NotImplementedError
+
+    @property
+    def x_intervals(self):
+        raise NotImplementedError
+
+    @property
+    def y_intervals(self):
+        raise NotImplementedError
+
+    @cached_property
+    def selected_objects(self):
+        """Filter out objects outside table boundaries"""
+
+        return [obj for obj in self.text_objects
+                if contains_or_overlap(self.table_bbox, obj.bbox)]
+
+
+class YGroupsAlgorithm(ExtractionAlgorithm):
+    """Extraction algorithm based on objects' y values"""
+
+    # TODO: filter out objects with empty text before grouping by y0 (but
+    # consider them if inside table's bbox)
+    # TODO: get y0 groups bbox and merge overlapping ones (overlapping only on
+    # y, not on x). ex: imgs-33281.pdf/06.png should not remove bigger cells
+
+    @cached_property
+    def table_bbox(self):
+        groups = group_objects(self.text_objects, self.y_threshold, 'y')
+        desired_objs = []
+        for group_objs in groups.values():
+            if len(group_objs) < 2:  # Ignore floating text objects
+                continue
+            desired_objs.extend(group_objs)
+        if not desired_objs:
+            return (0, 0, 0, 0)
+        x_min = min(obj.x0 for obj in desired_objs)
+        x_max = max(obj.x1 for obj in desired_objs)
+        y_min = min(obj.y0 for obj in desired_objs)
+        y_max = max(obj.y1 for obj in desired_objs)
+        return (x_min, y_min, x_max, y_max)
+
+    @staticmethod
+    def _define_intervals(objs, min_attr, max_attr, threshold, axis):
+        groups = group_objects(objs, threshold, axis)
+
+        intervals = [(key, max_attr(max(value, key=max_attr)))
+                    for key, value in groups.items()]
+        intervals.sort()
+        if not intervals:
+            return []
+
+        # Merge overlapping intervals
+        result = [intervals[0]]
+        for current in intervals[1:]:
+            previous = result.pop()
+            if current[0] <= previous[1] or current[1] <= previous[1]:
+                result.append((previous[0], max((previous[1], current[1]))))
+            else:
+                result.extend((previous, current))
+        return result
+
+    @cached_property
+    def x_intervals(self):
+        objects = self.selected_objects
+        objects.sort(key=lambda obj: obj.x0)
+        return self._define_intervals(
+            objects,
+            min_attr=lambda obj: obj.x0,
+            max_attr=lambda obj: obj.x1,
+            threshold=self.x_threshold,
+            axis='x',
+        )
+
+    @cached_property
+    def y_intervals(self):
+        objects = self.selected_objects
+        objects.sort(key=lambda obj: -obj.y1)
+        return self._define_intervals(
+            objects,
+            min_attr=lambda obj: obj.y0,
+            max_attr=lambda obj: obj.y1,
+            threshold=self.y_threshold,
+            axis='y',
+        )
+
+
+class RectsBoundariesAlgorithm(ExtractionAlgorithm):
+    """Extraction algorithm based on rectangles present in the page"""
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.rects = [obj for obj in self.objects
+                      if isinstance(obj, LTRect) and obj.fill]
+
+    @cached_property
+    def table_bbox(self):
+        y0 = min(obj.y0 for obj in self.rects)
+        y1 = max(obj.y1 for obj in self.rects)
+        x0 = min(obj.x0 for obj in self.rects)
+        x1 = max(obj.x1 for obj in self.rects)
+        return (x0, y0, x1, y1)
+
+    @staticmethod
+    def _clean_intersections(lines):
+
+        def other_line_contains(all_lines, search_line):
+            for line2 in all_lines:
+                if search_line == line2:
+                    continue
+                elif search_line[0] >= line2[0] and search_line[1] <= line2[1]:
+                    return True
+            return False
+
+        final = []
+        for line in lines:
+            if not other_line_contains(lines, line):
+                final.append(line)
+        return final
+
+    @cached_property
+    def x_intervals(self):
+        x_intervals = set((obj.x0, obj.x1) for obj in self.rects)
+        return sorted(self._clean_intersections(x_intervals))
+
+    @cached_property
+    def y_intervals(self):
+        y_intervals = set((obj.y0, obj.y1) for obj in self.rects)
+        return sorted(self._clean_intersections(y_intervals))
 
 
 def fill_matrix(objs, x_intervals, y_intervals):
@@ -219,121 +363,59 @@ def fill_matrix(objs, x_intervals, y_intervals):
     return matrix
 
 
-def get_table(objs, x_threshold=0.5, y_threshold=0.5):
+def get_table(objs, algorithm='y-groups', x_threshold=0.5, y_threshold=0.5):
     'Where the magic happens'
 
-    # TODO: split this function in many others
-    all_objs = objs
-    text_objs = [obj for obj in all_objs if isinstance(obj, TEXT_TYPES)]
+    # Define which extractor class to use
+    if algorithm == 'y-groups':
+        AlgorithmClass = YGroupsAlgorithm
+    elif algorithm == 'rects-boundaries':
+        AlgorithmClass = RectsBoundariesAlgorithm
+    objs = list(objs)
+    text_objs = [obj for obj in objs if isinstance(obj, TEXT_TYPES)]
+    extractor = AlgorithmClass(objs, text_objs, x_threshold, y_threshold)
 
-    # Define table lines based on objects' y values (with some threshold) -
-    # these lines will only be used to determine table boundaries.
-    lines = []
-    groups = group_objects(objs, lambda obj: obj.y0, y_threshold)
-    new_objs = []
-    for key, value in groups.items():
-        if len(value) < 2:  # Ignore 1-column tables and floating text objects
-            continue
-        value.sort(key=lambda obj: obj.x0)
-        lines.append((key, value))
-        new_objs.extend(value)
-
-    # Define table boundaries
-    x_min = y_min = float('inf')
-    x_max = y_max = float('-inf')
-    for _, line_objects in lines:
-        for obj in line_objects:
-            if obj.x0 <= x_min:
-                x_min = obj.x0
-            if obj.x1 >= x_max:
-                x_max = obj.x1
-            if obj.y0 <= y_min:
-                y_min = obj.y0
-            if obj.y1 >= y_max:
-                y_max = obj.y1
-    table_bbox = (x_min, y_min, x_max, y_max)
-
-    # Filter out objects outside table boundaries
-    objs = [obj for obj in text_objs
-            if contains_or_overlap(table_bbox, obj.bbox)]
-
-    # Define x and y intervals based on filtered objects
-    objs.sort(key=lambda obj: obj.x0)
-    x_intervals = define_intervals(
-            objs,
-            min_attr=lambda obj: obj.x0,
-            max_attr=lambda obj: obj.x1,
-            threshold=x_threshold
-    )
-    objs.sort(key=lambda obj: -obj.y1)
-    y_intervals = define_intervals(
-            objs,
-            min_attr=lambda obj: obj.y0,
-            max_attr=lambda obj: obj.y1,
-            threshold=y_threshold
+    # Fill the table based on x and y intervals from the extractor
+    return fill_matrix(
+        extractor.selected_objects,
+        extractor.x_intervals,
+        extractor.y_intervals,
     )
 
-    # Create an empty matrix and fill in with objects
-    return fill_matrix(objs, x_intervals, y_intervals)
 
-
-def pdf_table_lines(fobj, page_numbers, starts_after=None, ends_before=None):
-    # TODO: may use LTRect and LTLine objects to help identifying table
-    # boundaries and cells' positions when filling them.
+def pdf_table_lines(fobj, page_numbers, algorithm='y-groups',
+                    starts_after=None, ends_before=None):
     pages = pdf_objects(fobj, page_numbers, starts_after, ends_before)
-    header = None
+    header = line_size = None
     for page_index, page in enumerate(pages):
-        for line_index, line in enumerate(get_table(page)):
+        for line_index, line in enumerate(get_table(page, algorithm=algorithm)):
             if line_index == 0:
                 if page_index == 0:
                     header = line
+                    line_size = len(line)
                 elif page_index > 0 and line == header:  # skip header repetition
                     continue
+            assert line_size == len(line)
             yield line
-
-
-def pdf_to_text(filename_or_fobj):
-    'Extract all text objects from a PDF file'
-
-    filename, fobj = get_filename_and_fobj(filename_or_fobj, mode='rb')
-
-    # TODO: specify page range
-
-    parser = PDFParser(fobj)
-    doc = PDFDocument(parser)
-    parser.set_document(doc)
-    assert doc.is_extractable
-
-    for page in PDFPage.create_pages(doc):
-        rsrcmgr = PDFResourceManager()
-        laparams = LAParams()
-        result = io.StringIO()
-        device = TextConverter(rsrcmgr, result, laparams=laparams)
-        interpreter = PDFPageInterpreter(rsrcmgr, device)
-        interpreter.process_page(page)
-        yield result.getvalue()
 
 
 def import_from_pdf(filename_or_fobj, page_numbers=None,
                     starts_after=None, ends_before=None,
+                    algorithm='y-groups',
                     *args, **kwargs):
     filename, fobj = get_filename_and_fobj(filename_or_fobj, mode='rb')
 
     # TODO: create tests
     # TODO: pass threshold to pdf_table_lines (and from there to get_pages)
-    # TODO: specify page range
-    # TODO: filter out objects with empty text before grouping by y0 (but
-    # consider them if inside table's bbox)
-    # TODO: get y0 groups bbox and merge overlapping ones (overlapping only on
-    # y, not on x). ex: imgs-33281.pdf/06.png should not remove bigger cells
     meta = {
-            'imported_from': 'pdf',
-            'filename': filename,
+        'imported_from': 'pdf',
+        'filename': filename,
     }
     table_rows = pdf_table_lines(
         fobj,
         page_numbers,
         starts_after=starts_after,
         ends_before=ends_before,
+        algorithm=algorithm,
     )
     return create_table(table_rows, meta=meta, *args, **kwargs)
