95549f6c6845550345dace6c62d18440849906b5
Joao S O Bueno <gwidion@gmail.com>
2022-04-22 22:51:27 -0300

    [WIP] Tokenizer allowing queries to be input as strings

--- rows/utils/query.py
+++ rows/utils/query.py
@@ -18,12 +18,121 @@
 
 # from __future__ import unicode_literals
 
+import re
+
 from copy import deepcopy
+import operator
 import sys
 
 from collections.abc import Mapping
 
+def ensure_query(query):
+    if isinstance(query, str):
+        tokens = tokenize(query)
+        return Query._from_tokens(tokens)
+    if not isinstance(query, QueryBase):
+        raise TypeError(f"Can't create a query from a {type(query).__name__} instance.")
+    return query
 
+"""
+To think about:
+possibly plug-ins will want to add their own operators for the tokens
+(example: geometry operators "ABOVE", "BELLOW" for the PDF plugin)
+
+So, the token hierarchy will likely have to be contained as an object,
+which plug-ins can then create a copy, enhance, and change.
+"""
+
+
+class Token:
+    # these are not just "tokens": they will also be structured as Query tree nodes, and perform ops.
+    literal_registry = {}
+    match_registry = []
+    literal = None
+
+    def __new__(cls, value):
+
+        if value.upper() in __class__.literal_registry:
+            instance = super().__new__(__class__.literal_registry[value.upper()])
+        else:
+            for subcls in __class__.match_registry:
+                if subcls._match(value):
+                    instance = super().__new__(subcls)
+                    break
+            else:
+                raise ValueError(f"No registerd token type matches {value!r}")
+        # Python won't call __init__ if what __new__ returns is not an strict instance of __class__:
+        instance.__init__(value)
+        return instance
+
+    def __init__(self, value):
+        self.value = value
+
+    def __init_subclass__(cls, *args, **kw):
+        super().__init_subclass__(*args, **kw)
+        if cls.literal:
+            __class__.literal_registry[cls.literal.upper()] = cls
+        elif "_match" in cls.__dict__:
+            __class__.match_registry.append(cls)
+
+    @classmethod
+    def _match(cls, value):
+        raise NotImplementedError()
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}({self.value!r})"
+
+
+class BinOpToken(Token):
+    pass
+
+class EqualToken(BinOpToken):
+    literal = "=="
+    op = operator.eq
+
+# TODO: instantiate other operator token classes
+
+
+# IMPORTANT: Do not change declaration order of classes that do not declare a "literal" field.
+class LiteralToken(Token):
+    pass
+
+class FieldNameToken(Token):
+    @classmethod
+    def _match(cls, value):
+        return re.match(r"\w+", value) and not value[0].isdigit()
+
+class LiteralIntToken(LiteralToken):
+    def __init__(self, value):
+        self.value = int(value)
+
+    @classmethod
+    def _match(cls, value):
+        return re.match(r"\d+", value)
+
+class LiteralStrToken(Token):
+    def __init__(self, value):
+        self.value = value[1:-1]
+
+    @classmethod
+    def _match(cls, value):
+        return re.match(r"""(?P<quote>['"]).*?(?P=quote)""", value)
+
+
+def tokenize(query:str) -> "list[Token]":
+    tokens =  [Token(g[0]) for g in re.findall(r"""(OR|AND|\w+|((?P<quote>['"]).*?(?P=quote))|==|<|>|>=|<=)""", query, flags=re.IGNORECASE)]
+    return tokens
+
+
+class QueryBase:
+    @classmethod
+    def _from_tokens(cls, tokens):
+        raise NotImplementedError()
+
+class Query(QueryBase):
+    pass
+
+'''
 
 class Field:
     """Acessor class to identify a field (class attribute, dataclass field or dicionary key) to be used in queries
@@ -93,3 +202,5 @@ class Q:
 
     def _parse_ops(self, keyword_expressions)-> "list[tuple[op, Field, arg]]":
         pass
+'''
+
