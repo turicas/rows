75951276e3dcbbf5832911e3d2d8bbd1e314be3a
Joao S O Bueno <gwidion@gmail.com>
2022-06-09 22:26:54 -0300

    Initial sketch for OrdeableMapping

--- rows/table.py
+++ rows/table.py
@@ -32,6 +32,46 @@ from textwrap import dedent as D
 from .utils import query
 from .rows import CustomRowMixin
 
+from collections.abc import MutableMapping
+
+from threading import Lock
+
+class OrderableMapping(MutableMapping):
+    # maybe offload this to jsbueno/extradict?
+    def __init__(self, initial: "Union[Mapping, Sequence[tuple[hashable, any]]"):
+        self.data = {}
+        self.order = []
+        self.lock = Lock()
+        if isinstance (initial, Mapping):
+            initial = initial.items()
+        for key,value in initial:
+            self[key] = value
+
+    def __getitem__(self, key):
+        return self.data[key]
+
+    def __setitem__(self, key, value):
+        with self.lock:
+            if key not in self.data:
+                self.order.append(key)
+            self.data[key] = value
+
+    def __delitem__(self, key):
+        with self.lock:
+            del self.data[key]
+            self.order.remove(key)
+
+    def __len__(self):
+        return len(self.data)
+
+    def __iter__(self):
+        yield from iter(self.order)
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}({list(self.items())})"
+
+
+
 
 class BaseTable(MutableSequence, CustomRowMixin):
 
